1589694328ba7c528d01e5e9b136bf8710c2d163
cutsea110 <cutsea110@gmail.com>
2019-03-07 22:33:01 +0900

    ref.) http://www.kotha.net/hperf/ghc.html

--- Test.hs
+++ Test.hs
@@ -1,3 +1,14 @@
+{-# LANGUAGE MagicHash, UnboxedTuples #-}
 module Test where
 
-foo x = x
+import GHC.Prim
+import GHC.Exts
+
+cube :: Int -> Int
+cube x = x `timesInt` x `timesInt` x
+
+timesInt :: Int -> Int -> Int
+{-# NOINLINE timesInt #-}
+timesInt = \x y -> case x of
+  I# x' -> case y of
+    I# y' -> I# (x' *# y')
