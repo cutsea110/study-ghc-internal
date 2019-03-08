2d878f90621924492fa304cfd507baca95afb144
cutsea110 <cutsea110@gmail.com>
2019-03-09 08:41:38 +0900

    impliment naive

--- Some.hs
+++ Some.hs
@@ -4,20 +4,20 @@ import Control.Applicative (liftA2)
 import Data.Map (Map)
 import qualified Data.Map as M
 
-data InterMed k v = End
-                  | Val (Maybe k) (InterMed k v)
-                  | Key (Maybe v) (InterMed k v)
-                  | Nxt (Maybe (k, v)) (InterMed k v)
+data InterMed k v = BothEnd
+                  | ValEnd (Maybe k) (InterMed k v)
+                  | KeyEnd (Maybe v) (InterMed k v)
+                  | Next (Maybe (k, v)) (InterMed k v)
                   deriving (Show, Eq)
 
 foldIM :: (Ord k) =>
   ((t, a1 -> t -> t, a2 -> t -> t, t2 -> t -> t), (a1, k -> a1), (a2, v -> a2), (t2, (k, v) -> t2))
   -> InterMed k v -> t
 foldIM phi@((e,v,k,n),(c1,f1),(c2,f2),(c3,f3)) m = case m of
-  End -> e
-  Val a x -> v (foldMk  phi a) (foldIM phi x)
-  Key a x -> k (foldMv  phi a) (foldIM phi x)
-  Nxt a x -> n (foldMkv phi a) (foldIM phi x)
+  BothEnd -> e
+  ValEnd a x -> v (foldMk  phi a) (foldIM phi x)
+  KeyEnd a x -> k (foldMv  phi a) (foldIM phi x)
+  Next a x -> n (foldMkv phi a) (foldIM phi x)
 
 foldMk phi@((e,v,k,n),(c1,f1),(c2,f2),(c3,f3)) m = case m of
   Nothing -> c1
@@ -31,27 +31,43 @@ foldMkv phi@((e,v,k,n),(c1,f1),(c2,f2),(c3,f3)) m = case m of
   Nothing -> c3
   Just a  -> f3 a
 
-data X k v a = E
-             | V (Maybe k) a
-             | K (Maybe v) a
-             | N (Maybe (k, v)) a
+data MaybeIM k v a = BE
+             | VE (Maybe k) a
+             | KE (Maybe v) a
+             | NT (Maybe (k, v)) a
              deriving (Show, Eq)
 
 unfoldIM psis@(psi, psi1, psi2, psi3) x = case psi x of
-  E       -> End
-  V mk  m -> Val (unfoldMk  psis mk)  (unfoldIM psis m)
-  K mv  m -> Key (unfoldMv  psis mv)  (unfoldIM psis m)
-  N mkv m -> Nxt (unfoldMkv psis mkv) (unfoldIM psis m)
+  BE       -> BothEnd
+  VE mk  m -> ValEnd (unfoldMk  psis mk)  (unfoldIM psis m)
+  KE mv  m -> KeyEnd (unfoldMv  psis mv)  (unfoldIM psis m)
+  NT mkv m -> Next (unfoldMkv psis mkv) (unfoldIM psis m)
 
 unfoldMk  psis@(psi, psi1, psi2, psi3) x = psi1 x
 unfoldMv  psis@(psi, psi1, psi2, psi3) x = psi2 x
 unfoldMkv psis@(psi, psi1, psi2, psi3) x = psi3 x
 
-psi :: ([Maybe k], [Maybe v]) -> X k v ([Maybe k], [Maybe v])
-psi ([],[])      = E
-psi (k:ks, [])   = V k (ks, [])
-psi ([], v:vs)   = K v ([], vs)
-psi (k:ks, v:vs) = N (liftA2 (,) k v) (ks, vs)
+psi :: ([Maybe k], [Maybe v]) -> MaybeIM k v ([Maybe k], [Maybe v])
+psi ([],[])      = BE
+psi (k:ks, [])   = VE k (ks, [])
+psi ([], v:vs)   = KE v ([], vs)
+psi (k:ks, v:vs) = NT (liftA2 (,) k v) (ks, vs)
+
+psis = (psi, id, id, id)
+{-
+phi = (e, v, k, n)
+  where
+    e = Just M.empty
+    v Nothing _ = Nothing
+    v (Just _) z = z
+    k Nothing _ = Nothing
+    k (Just _) z = z
+    n Nothing _ = Nothing
+    n (Just (k, v)) z = undefined -- Just (M.insert k v z)
+-}
+-- phis = (phi, id, id, id)
+
+-- some = foldIM phis . unfoldIM psis
 
 {-
 some :: (Ord k) => ([Maybe k], [Maybe v]) -> Maybe (M.Map k v)
