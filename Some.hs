258344fd85c402f49558cf3c1197d08168274411
cutsea110 <cutsea110@gmail.com>
2019-03-08 10:56:55 +0900

    refine

--- Some.hs
+++ Some.hs
@@ -1,27 +1,33 @@
 module Some where
 
+import Control.Applicative (liftA2)
 import Data.Map (Map)
 import qualified Data.Map as M
 
-some :: Ord k => ([Maybe k], [Maybe v]) -> Maybe (Map k v)
-some = hylo phi z psi
-  where
-    z = Just M.empty
-    phi (Just Nothing, _) _ = Nothing
-    phi (_, Just Nothing) _ = Nothing
-    phi _ Nothing = Nothing
-    phi (Nothing, _) _     = z
-    phi (_, Nothing) _     = z
-    phi (Just (Just k), Just (Just v)) (Just m) = Just (M.insert k v m)
-    psi ([], [])   = Nothing
-    psi (k:ks, []) = Just ((Just k, Nothing), (ks, []))
-    psi ([], v:vs) = Just ((Nothing, Just v), ([], vs))
-    psi (k:ks,v:vs) = Just ((Just k, Just v), (ks,vs))
-    
-hylo :: (b -> c -> c) -> c -> (a -> Maybe (b, a)) -> a -> c
-hylo phi z psi x = case psi x of
-  Nothing      -> z
-  Just (y, x') -> phi y (hylo phi z psi x')
+data InterMed k v = BothEnd
+                  | ValEnd (Maybe k) (InterMed k v)
+                  | KeyEnd (Maybe v) (InterMed k v)
+                  | Next (Maybe (k, v)) (InterMed k v)
+                  deriving (Show, Eq)
+
+phi :: (Ord k) => M.Map k v -> InterMed k v -> Maybe (M.Map k v)
+phi z BothEnd = Just z
+phi _ (ValEnd Nothing _)  = Nothing
+phi z (ValEnd (Just _) r) = phi z r
+phi _ (KeyEnd Nothing _)  = Nothing
+phi z (KeyEnd (Just _) r) = phi z r
+phi _ (Next Nothing _)    = Nothing
+phi z (Next (Just (k, v)) r) = phi (M.insert k v z) r
+
+psi :: ([Maybe k], [Maybe v]) -> InterMed k v
+psi ([], []) = BothEnd
+psi (mk:mks, []) = ValEnd mk (psi (mks, []))
+psi ([], mv:mvs) = KeyEnd mv (psi ([], mvs))
+psi (mk:mks, mv:mvs) = Next (liftA2 (,) mk mv) (psi (mks, mvs))
+
+some :: (Ord k) => ([Maybe k], [Maybe v]) -> Maybe (M.Map k v)
+some = phi z . psi
+  where z = M.empty
 
 {-
 pairWith f (Just x) (Just y) = Just (f x y)
@@ -60,4 +66,11 @@ vals3 = [Just 'a', Just 'b', Just 'c']
 
 keys4 = [Just 1, Just 2, Just 3]
 vals4 = [Just 'a', Just 'b', Just 'c', Nothing]
+
+keys5 = [Just 1, Just 2, Just 3]
+vals5 = [Just 'a', Just 'b', Just 'c', Just 'd', Just 'e']
+
+keys6 = [Just 1, Just 2, Just 3]
+vals6 = [Just 'a', Just 'b', Just 'c', Nothing, Just 'e']
+
 
