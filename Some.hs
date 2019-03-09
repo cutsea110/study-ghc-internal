38acd9c65532d0ac42ff9e1fd6c7cda7225e8c76
cutsea110 <cutsea110@gmail.com>
2019-03-09 14:21:21 +0900

    [Refactor]

--- Some.hs
+++ Some.hs
@@ -73,33 +73,7 @@ some = foldIM phis . unfoldIM psis
   where
     psis = (psi, id, id, id)
     phis = (phi, (Nothing, Just), (Nothing, Just), (Nothing, Just))
-    
 
-{-
-some :: (Ord k) => ([Maybe k], [Maybe v]) -> Maybe (M.Map k v)
-some = foldIM phi . unfoldIM psi
-  where
-    phi = undefined
-    psi = undefined
--}
-
-{-
-pairWith f (Just x) (Just y) = Just (f x y)
-pairWith _ _        _        = Nothing
-
-pair = pairWith (,)
-left = pairWith const
-cross f g (x, y) = (f x, g y)
-
-some :: [Maybe a] -> [Maybe b] -> Maybe [(a, b)]
-some xs ys = sequence ps `left` sequence rs
-  where
-    dummy = repeat (Just undefined)
-    (ps, rs) = go xs ys []
-    go (x:xs) (y:ys) ps = cross (pair x y:) id $ go xs ys ps
-    go []     ys     ps = (ps, zipWith pair dummy ys)
-    go xs     []     ps = (ps, zipWith pair xs dummy)
--}
 keys = [Just 1, Just 2, Just 3]
 vals = [Just 'a', Just 'b', Just 'c']
 
